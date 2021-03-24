8ff323530c3c3132bb0839f3401b2d9a99e464f8
Ulysse Tallepied <ulysse.tallepied@hetic.net>
2021-03-24 16:54:31 +0100

    Add classnames to TW example

--- components/Button_Tailwind.js
+++ components/Button_Tailwind.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-
+import classnames from "classnames";
 export default function ButtonTailwind({
   importance = "primary",
   size = "normal",
@@ -7,26 +7,22 @@ export default function ButtonTailwind({
   rootStyle = "",
 }) {
   const [clicked, setClicked] = useState(false);
-
   return (
     <button
-      className={`border border-transparent rounded-md focus:outline-none font-medium transform hover:scale-110 transition-all
-        ${
-          importance === "primary" &&
-          `text-white ${!clicked ? "bg-indigo-600 hover:bg-indigo-800" : "bg-red-600 hover:bg-red-800 text-white"}`
-        }
-        ${
-          importance === "secondary" &&
-          `bg-transparent border border-solid ${
-            !clicked
-              ? "border-indigo-600 hover:border-indigo-800 text-indigo-600 hover:text-indigo-800"
-              : "border-red-600 hover:border-red-800 text-red-600 hover:text-red-800"
-          }`
-        }
-        ${size === "normal" && "px-2 py-1 text-base"}
-        ${size === "big" && "px-4 py-3 text-xl"}
-        ${rootStyle}
-      `}
+      className={classnames(
+        rootStyle,
+        "border rounded-md focus:outline-none font-medium transform hover:scale-110 transition-all",
+        importance === "primary" && !clicked && "text-white bg-indigo-600 hover:bg-indigo-800",
+        importance === "primary" && clicked && "text-white bg-red-600 hover:bg-red-800",
+        importance === "secondary" &&
+          !clicked &&
+          "bg-transparent border border-solid border-indigo-600 hover:border-indigo-800 text-indigo-600 hover:text-indigo-800",
+        importance === "secondary" &&
+          clicked &&
+          "bg-transparent border border-solid border-red-600 hover:border-red-800 text-red-600 hover:text-red-800",
+        size === "normal" && "px-2 py-1 text-base",
+        size === "big" && "px-4 py-3 text-xl"
+      )}
       onClick={() => setClicked(!clicked)}
       type="button"
     >
