da2b0cdcf38711a0b3e76c98259e26be39552920
Bruno Stasse <bruno.stasse@gmail.com>
2021-03-24 10:27:46 +0100

    Add `css` function for Emotion

--- components/StyledButton.js
+++ components/StyledButton.js
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import styled from "@emotion/styled";
 
+const css = (string) => string;
+
 export default function ButtonTailwind({ importance = "primary", size = "normal", children = "Click me" }) {
   const [clicked, setClicked] = useState(false);
   const buttonType = clicked ? "clicked" : importance;
@@ -27,12 +29,37 @@ const Button = styled.button`
     /* ${(props) => (!!props.clicked ? `background-color: rgb(185, 28, 28);` : "")}; */
     /* &:hover { background-color: rgb(199, 210, 254)} */
   }
-  ${(props) => (props.buttonType === "primary" ? `background-color: rgb(79, 70, 229); color: white` : "")};
   ${(props) =>
-    props.buttonType === "secondary" ? `background-color: rgb(224, 231, 255); color: rgb(67, 56, 202); ` : ""};
-  ${(props) => (props.size === "big" ? `padding: 0.4rem 0.8rem; font-size: 1.25rem` : "")};
+    props.buttonType === "primary"
+      ? css`
+          background-color: rgb(79, 70, 229);
+          color: white;
+        `
+      : ""};
   ${(props) =>
-    props.clicked ? `background-color: rgba(220, 38, 38); color: white; &:hover { background-color : orange;};` : ""};
+    props.buttonType === "secondary"
+      ? css`
+          background-color: rgb(224, 231, 255);
+          color: rgb(67, 56, 202);
+        `
+      : ""};
+  ${(props) =>
+    props.size === "big"
+      ? css`
+          padding: 0.4rem 0.8rem;
+          font-size: 1.25rem;
+        `
+      : ""};
+  ${(props) =>
+    props.clicked
+      ? css`
+          background-color: rgba(220, 38, 38);
+          color: white;
+          &:hover {
+            background-color: orange;
+          }
+        `
+      : ""};
   &:focus {
     outline: 2px solid transparent;
     outline-offset: 2px;
